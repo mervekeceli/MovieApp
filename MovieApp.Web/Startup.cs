1fc54443575c12763cb0d487445984464d0f4168
mervekeceli <mervekeceli55e5@gmail.com>
2024-11-10 00:32:38 +0300

    Efcore Context işlemleri yapıldı

--- MovieApp.Web/Startup.cs
+++ MovieApp.Web/Startup.cs
@@ -1,9 +1,12 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.FileSystemGlobbing.Internal.Patterns;
 using Microsoft.Extensions.Hosting;
+using MovieApp.Web.Data;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,10 +16,20 @@ namespace MovieApp.Web
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<MovieContext>(options => 
+                options.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
             services.AddControllersWithViews(); // MVC yapýsýný projemizde kullanacaðýmýzý belirtiyoruz.
         }
 
