99de84a0cdae36108fea49ba4d16891bff4fdae8
mervekeceli <mervekeceli55e5@gmail.com>
2024-11-15 20:48:47 +0300

    LİNQ sorguları yazıldı

--- LinqSamples/Data/NorthwindContext.cs
+++ LinqSamples/Data/NorthwindContext.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.Extensions.Logging;
 
 namespace LinqSamples.Data
 {
@@ -15,6 +16,8 @@ namespace LinqSamples.Data
             : base(options)
         {
         }
+        public static readonly ILoggerFactory MyLoggerFactory
+            = LoggerFactory.Create(builder => { builder.AddConsole(); });
 
         public virtual DbSet<AlphabeticalListOfProduct> AlphabeticalListOfProducts { get; set; } = null!;
         public virtual DbSet<Category> Categories { get; set; } = null!;
@@ -48,8 +51,9 @@ namespace LinqSamples.Data
         {
             if (!optionsBuilder.IsConfigured)
             {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Database=Northwind;Integrated Security=SSPI;");
+                optionsBuilder
+                    .UseLoggerFactory(MyLoggerFactory)
+                    .UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Database=Northwind;Integrated Security=SSPI;");
             }
         }
 
--- LinqSamples/Program.cs
+++ LinqSamples/Program.cs
@@ -1,5 +1,140 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using LinqSamples.Data;
 
 //DB first için gerekli migration işlemi: 
 //dotnet ef dbcontext scaffold "Data Source=(localdb)\\MSSQLLocalDB;Database=Northwind;Integrated Security=SSPI;" "Microsoft.EntityFrameworkCore.SqlServer" --output-dir "Data" --context NorthwindContext
+#region Select Sorguları
+using (var db = new NorthwindContext())
+{
+    
+    //var products = db.Products.ToList();
+    //var products = db.Products.Select(p => new { p.ProductName, p.Category }).ToList();
+
+    //var product = db.Products.First();
+    //Console.WriteLine(product.ProductName + " -> " + product.UnitPrice);
+
+    //var product1 = db.Products.Select(p=>new { p.ProductName, product.UnitPrice }).FirstOrDefault();
+    //Console.WriteLine(product1.ProductName + " -> " + product1.UnitPrice);
+
+    //foreach(var product in products)
+    //{
+    //    console.writeline(product.productname + " -> " + product.category.categoryname);
+    //}     
+}
+#endregion
+
+#region Filtreleme Sorguları
+
+//using (var db = new NorthwindContext())
+//{
+//    //var products = db.Products.Where(p => p.UnitPrice > 18 && p.UnitPrice < 30).ToList();
+//    //var products = db.Products.Select(x=>new { x.ProductName, x.UnitPrice}).Where(p => p.UnitPrice > 18 && p.UnitPrice < 30).ToList();
+//    //var products = db.Products.Where(p => p.CategoryId == 1).ToList();
+//    //var products = db.Products.Where(p => p.CategoryId > 1 && p.CategoryId < 5).ToList();
+//    //var products = db.Products.Where(p => p.CategoryId == 1 || p.CategoryId == 5).ToList();
+
+//    //var products = db.Products.Where(i => i.ProductName == "Chai").ToList();
+//    var products = db.Products.Where(i => i.ProductName.Contains("Ch")).ToList();
+//    foreach (var product in products)
+//    {
+//        Console.WriteLine(product.ProductName + ' ' + product.UnitPrice);
+//    }
+//}
+
+#endregion
+
+
+#region Linq Select Sorgu Uygulamaları
+
+//Tüm müşteri kayıtlarını getiriniz.
+/*using (var db = new NorthwindContext())
+{
+    var customers = db.Customers.ToList();
+
+    foreach (var customer in customers)
+    {
+        Console.WriteLine(customer.CompanyName);
+    }
+}*/
+
+
+//Tüm müşterilerin sadece customerId ve ContactName klonlarını getiriniz.
+/*using (var db = new NorthwindContext())
+{
+    var customers = db.Customers.Select(x => new { x.ContactName, x.CustomerId }).ToList();
+
+    foreach (var customer in customers)
+    {
+        Console.WriteLine(customer.ContactName + " -> " + customer.CustomerId);
+    }
+}*/
+
+
+//Almanya'da yaşayan müşterilerin adlarını getiriniz.
+/*using (var db = new NorthwindContext())
+{
+    var customers = db.Customers.Where(x=>x.Country == "Germany").ToList();
+
+    foreach (var customer in customers)
+    {
+        Console.WriteLine(customer.CompanyName);
+    }
+}*/
+
+
+//"Diego Roel" isimli müşteri nerde yaşamaktadır?
+/*using (var db = new NorthwindContext())
+{
+    var customer = db.Customers.Where(x => x.ContactName == "Diego Roel").FirstOrDefault();
+    Console.WriteLine(customer.Address);
+}*/
+
+
+//Stokta olmayan ürünler hangileridir?
+/*using(var db = new NorthwindContext())
+{
+    var noStockProducts = db.Products
+        .Select(a => new { a.ProductName, a.UnitsInStock })
+        .Where(i => i.UnitsInStock == 0).ToList();
+
+    foreach (var product in noStockProducts)
+    {
+        Console.WriteLine(product.ProductName);
+    }
+}*/
+
+//Tüm çalışanların ad ve soyadını tek kolon şeklinde getiriniz.
+/*using(var db = new NorthwindContext())
+{
+    var employee = db.Employees.Select(i => new {FullName = i.FirstName + " " + i.LastName }).ToList();
+
+    foreach(var e in employee)
+    {
+        Console.WriteLine(e.FullName);
+    }
+}*/
+
+//Ürünler tablosundaki ilk beş kaydı alınız.
+
+//using(var  db = new NorthwindContext())
+//{
+//    var products = db.Products.Take(5).ToList();
+
+//    foreach (var product in products)
+//    {
+//        Console.WriteLine(product.ProductName+ " " + product.ProductId);
+//    }
+//}
+//Ürünler tablosundaki ikini 5 kaydı alınız
+using (var db = new NorthwindContext())
+{
+    var products = db.Products.Skip(5).Take(5).ToList(); //Skip: öteleme işlemi
+
+    foreach (var product in products)
+    {
+        Console.WriteLine(product.ProductName + " " + product.ProductId);
+    }
+}
+
+#endregion
+Console.ReadLine();
