0af76ca964663a37a2a48f4236692e7457d89b5e
mervekeceli <mervekeceli55e5@gmail.com>
2024-11-20 02:34:55 +0300

    Admin paneli - Film güncelleme işlemleri yapıldı

--- MovieApp.Web/Controllers/AdminController.cs
+++ MovieApp.Web/Controllers/AdminController.cs
@@ -19,12 +19,54 @@ namespace MovieApp.Web.Controllers
             return View();
         }
 
+        public IActionResult MovieUpdate(int? id)
+        {
+            if (id == null)
+                return NotFound();
+
+            var entity = _context.Movies.Select(m => new AdminEditMovieViewModel
+            {
+                MovieId = m.MovieId,
+                Title = m.Title,
+                Description = m.Description,
+                ImageUrl = m.ImageUrl,
+            }).FirstOrDefault(m=>m.MovieId == id);
+
+            if (entity == null)
+                return NotFound();
+
+            return View(entity);
+        }
+
+        [HttpPost]
+        public IActionResult MovieUpdate(AdminEditMovieViewModel model)
+        {
+            var entity = _context.Movies.Find(model.MovieId);
+            if(entity == null)
+                return NotFound();
+
+            entity.Title = model.Title;
+            entity.Description = model.Description;
+            entity.ImageUrl = model.ImageUrl;
+
+            _context.SaveChanges();
+
+            return RedirectToAction("MovieList");
+        }
+
         public IActionResult MovieList()
         {
             return View(new AdminMoviesViewModel
             {
                 Movies = _context.Movies
                 .Include(m => m.Genres)
+                //.Select(x=> new AdminMovieViewModel
+                //{
+                //    MovieId = x.MovieId,
+                //    Title = x.Title,
+                //    ImageUrl = x.ImageUrl,
+                //    Genres = x.Genres.ToList()
+                //})
                 .ToList()
             });
         }
