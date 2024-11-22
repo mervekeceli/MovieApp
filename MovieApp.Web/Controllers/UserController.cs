6ff25dfa8bbc69e3a04bc975696388877f714eda
mervekeceli <mervekeceli55e5@gmail.com>
2024-11-22 05:01:19 +0300

    Form validation-kullanıcı ekleme formu eklendi

---
+++ MovieApp.Web/Controllers/UserController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MovieApp.Web.Model;
+
+namespace MovieApp.Web.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult CreateUser()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult CreateUser(UserModel model)
+        {
+            return View();
+        }
+    }
+}
---
+++ MovieApp.Web/Model/UserModel.cs
@@ -0,0 +1,14 @@
+﻿namespace MovieApp.Web.Model
+{
+    public class UserModel
+    {
+        public int UserId { get; set; }
+        public string UserName { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string RePassword { get; set; }
+        public string Url { get; set; }
+        public int BirthYear { get; set; }
+    }
+}
